0e54b3efc9ef47d161d0ae6eeea7de9130629998
tekin <gundogdutekin@gmail.com>
2022-09-14 17:53:03 +0300

    Partials Files created in view folder

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 import express from 'express';
+import ejs from 'ejs';
 
 const app = express();
 
-const port = 3000;
-
+//Template Engine "EJS" Set
+app.set('view engine', 'ejs');
+//MIDDLEWARES
+//Static Files Middleware
+app.use(express.static('public'));
+//ROUTES
 app.get('/', (req, res) => {
-    res.send('merhaba');
+    res.status(200).render('index', {
+        page_name: 'index',
+    });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
 });
 
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server ${port} portunda başlatıldı.`);
 });
