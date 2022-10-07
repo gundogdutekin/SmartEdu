3c407759a025d71dbdf534c4603efa434d9e221c
tekin <gundogdutekin@gmail.com>
2022-10-07 20:30:08 +0300

    Created redirect middleware

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getIndexPage, getAboutPage, getRegisterPage, getLoginPage } from '../controllers/pageController.js';
+import redirectMiddleware from "../middlewares/redirectMiddleware.js";
 
 const pageRouter = express.Router();
 
 pageRouter.route('/').get(getIndexPage);
 pageRouter.route('/about').get(getAboutPage);
-pageRouter.route('/register').get(getRegisterPage);
+pageRouter.route('/register').get(redirectMiddleware,getRegisterPage);
 pageRouter.route('/login').get(getLoginPage);
 
 export { pageRouter };
